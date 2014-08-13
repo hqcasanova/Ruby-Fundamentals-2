9117e75067f03a3ed85640aeb801fecf0434d0da
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 09:34:34 -0400

    Prompting the user for temp

---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+#Method for converting Farenheit degrees into Celsius degrees
+# def convert_temp(temp_f)
+# 	return (temp_f - 32) * 5/9
+# end
+
+#Prompt the user for a temperature in F
+print "Please, provide the current temperature in Farenheit units: "
+temp_f = gets.chomp.to_i
+#convert_temp(temp_f)
+
