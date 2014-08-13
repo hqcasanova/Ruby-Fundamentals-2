3815096444042ee37ff92363888ccad356085f8c
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 13:44:19 -0400

    Printing out list of groceries

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,7 @@
 #Shopping list
 grocery_list = ['zuchinni', 'carrots', 'potatoes', 'green beans', 'leek', 'onion', 'garlic', 'sea salt']
+
+#Print out the list as a real list
+grocery_list.each do |element|
+  puts "* " + element
+end
