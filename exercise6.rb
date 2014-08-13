ec1438c43b3591de02965ed4f34da73ea65172bd
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 13:49:53 -0400

    Print method created and rice added

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,12 @@
 grocery_list = ['zuchinni', 'carrots', 'potatoes', 'green beans', 'leek', 'onion', 'garlic', 'sea salt']
 
 #Print out the list as a real list
-grocery_list.each do |element|
-  puts "* " + element
+def print_list (list)
+  list.each do |element|
+    puts "* " + element
+  end
 end
+
+#Adding rice and printing out
+grocery_list << 'rice'
+print_list(grocery_list)
