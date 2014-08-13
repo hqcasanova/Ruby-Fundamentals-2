065d4cdfbf51dcedda71c969b366e72fe3697d20
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 13:54:32 -0400

    Added option for count

--- exercise6.rb
+++ exercise6.rb
@@ -2,10 +2,11 @@
 grocery_list = ['zuchinni', 'carrots', 'potatoes', 'green beans', 'leek', 'onion', 'garlic', 'sea salt']
 
 #Print out the list as a real list
-def print_list (list)
+def print_list (list, print_total = true)
   list.each do |element|
     puts "* " + element
   end
+  puts 'The total number of items is ' + list.length.to_s if print_total
 end
 
 #Adding rice and printing out
