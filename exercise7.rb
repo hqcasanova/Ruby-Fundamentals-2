e82cbc9179b9cd88d9c5a7e89db4ee43d392f1a5
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 14:47:55 -0400

    Add (cohort4, 43) pair

--- exercise7.rb
+++ exercise7.rb
@@ -1,11 +1,15 @@
 #Cohorts and their size
 students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
 
-#Display name and number of students
+#Display name of cohorts and their respective sizes
 def print_list(list, units)
   list.each do |key, value|
     puts "#{key}: #{value} #{units}"
   end
+  print "\n"
 end
+print_list(students, 'students')
 
+#Add pair (cohort4, 43)
+students[:cohort4] = 43
 print_list(students, 'students')
