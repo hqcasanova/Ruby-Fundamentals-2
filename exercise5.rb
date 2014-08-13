36317586935742fcbf3f97f2b5600481b8b2df5a
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-13 09:54:14 -0400

    Added support for decimals. Proof-read

--- exercise5.rb
+++ exercise5.rb
@@ -1,10 +1,10 @@
-#Method for converting Farenheit degrees into Celsius degrees
+#Method for converting Fahrenheit degrees into Celsius degrees
 def convert_temp(temp_f)
-  return (temp_f - 32) * 5/9
+  return (temp_f - 32) * 5/9.0
 end
 
 #Prompt the user for a temperature in F
-print "Please, provide the current temperature in Farenheit units: "
+print "Please, provide the current temperature in Fahrenheit units: "
 temp_f = gets.chomp.to_i
 puts "The current temperature in Celsius is #{convert_temp(temp_f)}"
 
